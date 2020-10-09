818a5c2ae6e9280b84420f04d36fc0c17d975580
priyarajeshranjan <rranja01@gmail.com>
2020-10-09 15:54:43 -0700

    context manager database added

--- utils/database_sql.py
+++ utils/database_sql.py
@@ -1,55 +1,43 @@
-import sqlite3
+from .database_connt import DatabaseConnection
+
 #    concern with retrieving and storing book from a database
 
 
 def create_book_table():
-   connection = sqlite3.connect('data.db')
-   cursor = connection.cursor()
+  with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-   cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key,author text,read integer)')
-
-   connection.commit()
-   connection.close()
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key,author text,read integer)')
 
 def get_all_books():
-   connection = sqlite3.connect('data.db')
-   cursor = connection.cursor()
+   with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-   cursor.execute('SELECT * FROM books')
-   books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
-
-   connection.close()
+        cursor.execute('SELECT * FROM books')
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
 
    return books
 
 def add_book(name, author):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+         cursor = connection.cursor()
 
-    cursor.execute(f'INSERT INTO books VALUES(?,?,0)',(name,author))
-
-    connection.commit()
-    connection.close()
+         cursor.execute(f'INSERT INTO books VALUES(?,?,0)',(name,author))
 
 
 def mark_book_as_read(name):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('UPDATE books SET read=1 WHERE name=?',(name,))
-
-    connection.commit()
-    connection.close()
-
+        cursor.execute('UPDATE books SET read=1 WHERE name=?',(name,))
 
 def delete_book(name):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+       cursor = connection.cursor()
 
-    cursor.execute('DELETE FROM books WHERE name=?',(name,))
+       cursor.execute('DELETE FROM books WHERE name=?',(name,))
 
-    connection.commit()
-    connection.close()
+
 
 
 # def delete_book(name):
